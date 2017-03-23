86160921d5490610192f2e6ee80fd27481a16dae
kevin <kevinpuyol@live.fr>
2017-03-23 11:23:50 +0100

    exo1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo1</title>
+</head>
+<body>
+	<?php 
+
+	echo $_SERVER['HTTP_USER_AGENT'].'</br> ';
+	echo $_SERVER["REMOTE_ADDR"].'</br>';
+	echo $_SERVER["SERVER_NAME"].'</br> ';
+
+	?>
+</body>
+</html>
